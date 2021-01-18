1284912247fd372838ab2250f1b0ac746307de94
CELINE <61349004+celineworks@users.noreply.github.com>
2021-01-18 21:19:10 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'celine-test',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  generate: {
+    async routes() {
+      const pages = await axios
+        .get('https://celine-test.microcms.io//api/v1/blog?limit=100', {
+          headers: { 'X-API-KEY': '9119be3c-501d-456d-a158-0d2025a88549' }
+        })
+        .then((res) =>
+          res.data.contents.map((content) => ({
+            route: `/${content.id}`,
+            payload: content
+          }))
+        )
+      return pages
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      'https://celine-test.microcms.io//api/v1/blog',
+      {
+        headers: { 'X-API-KEY': '9119be3c-501d-456d-a158-0d2025a88549' }
+      }
+    )
+    return data
+  }
+}
+</script>
