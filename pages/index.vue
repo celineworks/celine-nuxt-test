3168d1a35695bb5c7437e0475e269983cc91b3b7
CELINE <61349004+celineworks@users.noreply.github.com>
2021-01-18 21:30:55 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@ import axios from 'axios'
 export default {
   async asyncData() {
     const { data } = await axios.get(
-      'https://celine-test.microcms.io//api/v1/blog',
+      'https://celine-test.microcms.io/api/v1/blog',
       {
         headers: { 'X-API-KEY': '9119be3c-501d-456d-a158-0d2025a88549' }
       }
